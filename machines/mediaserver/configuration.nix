3fb239fdbc998ba3144af2387cf49c5d9c6a494d
Maxwell L-T <maxwell.lt@live.com>
2020-08-13 22:06:31 -0400

    Setup nginx reverse proxy for Jellyfin over HTTPS

--- machines/mediaserver/configuration.nix
+++ machines/mediaserver/configuration.nix
@@ -21,6 +21,31 @@
   networking.useDHCP = false;
   networking.interfaces.enp0s31f6.useDHCP = true;
 
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  services.nginx = {
+    enable = true;
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+
+    virtualHosts."media.maxwell-lt.dev" = {
+      addSSL = true;
+      enableACME = true;
+      locations."/" = {
+        proxyPass = "http://localhost:8096";
+      };
+    };
+  };
+
+  security.acme = {
+    acceptTerms = true;
+    certs = {
+      "media.maxwell-lt.dev".email = "maxwell.lt@live.com";
+    };
+  };
+
   # Don't change this value from 20.03!
   system.stateVersion = "20.03"; # Did you read the comment?
 }
