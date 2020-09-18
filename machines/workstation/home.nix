82dc6b07531fa36572806bcf1fa40e8c293be52a
Maxwell L-T <maxwell.lt@live.com>
2020-09-17 23:58:15 -0400

    Add Neovim and some configuration

--- machines/workstation/home.nix
+++ machines/workstation/home.nix
@@ -1,5 +1,17 @@
 { config, pkgs, ... }:
 
+let
+  breezy = pkgs.vimUtils.buildVimPluginFrom2Nix {
+    pname = "breezy";
+    version = "2020-03-24";
+    src = pkgs.fetchFromGitHub {
+      owner = "fneu";
+      repo = "breezy";
+      rev = "453167dc346f39e51141df4fe7b17272f4833c2b";
+      sha256 = "09w4glff27sw4z2998gpq5vmlv36mfx9vp287spm7xvaq9fnn6gb";
+    };
+  };
+in
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
@@ -7,10 +19,50 @@
   home.packages = with pkgs; [
     #lutris
     playonlinux
+    ripgrep
   ];
 
   nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
   nixpkgs.config.allowUnfree = true;
+
+    programs = {
+    neovim = {
+      enable = true;
+      viAlias = true;
+      vimAlias = true;
+      withNodeJs = true;
+      extraConfig = ''
+        filetype plugin indent on
+	syntax on
+	set backspace=indent,eol,start
+	set hidden
+	set nobackup
+	set nowritebackup
+	set noswapfile
+	set cmdheight=2
+	set updatetime=300
+        set shortmess+=c
+        colorscheme breezy
+        set background=light
+
+        let g:lightline = {
+          \ 'colorscheme': 'breezy',
+          \ 'component': {
+          \   'readonly': '%{&readonly?"":""}',
+          \ },
+          \ 'separator': { 'left': '', 'right': '' },
+          \ 'subseparator': { 'left': '', 'right': '' }
+          \ }
+       '';
+      plugins = with pkgs.vimPlugins; [
+        lightline-vim coc-nvim vim-nix fzf-vim coc-fzf fzfWrapper breezy
+      ];
+    };
+    fzf = {
+      enable = true;
+      defaultCommand = "rg --files --no-ignore-vcs --hidden";
+    };
+  };
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
