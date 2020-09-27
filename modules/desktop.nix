8c385199b3419abef04ab9f95d917f06e744db1c
Maxwell L-T <maxwell.lt@live.com>
2020-09-27 16:38:00 -0400

    Remove overlay for yEd, version bump is now in unstable

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -53,19 +53,6 @@ in
     };
   };
 
-  nixpkgs.overlays = [
-    (self: super: {
-      yed = super.yed.overrideAttrs (old: {
-        pname = "yEd";
-        version = "3.20.1";
-        src = super.fetchzip {
-          url = "https://www.yworks.com/resources/yed/demo/yEd-3.20.1.zip";
-          sha256 = "0sd73s700f3gqq5zq1psrqjg6ff2gv49f8vd37v6bv65vdxqxryq";
-        };
-      });
-    })
-  ];
-
   # Enable IME
   i18n.inputMethod = {
     enabled = "fcitx";
