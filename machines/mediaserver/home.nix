8e3a8c9f6da79f9774d3da88b1740b17873c68a5
Maxwell L-T <maxwell.lt@live.com>
2020-10-13 21:15:37 -0400

    Merge and improve neovim configuration

--- machines/mediaserver/home.nix
+++ machines/mediaserver/home.nix
@@ -1,24 +1,11 @@
 { config, pkgs, ... }:
 
-let
-  breezy = pkgs.vimUtils.buildVimPluginFrom2Nix {
-    pname = "breezy";
-    version = "2020-03-24";
-    src = pkgs.fetchFromGitHub {
-      owner = "fneu";
-      repo = "breezy";
-      rev = "453167dc346f39e51141df4fe7b17272f4833c2b";
-      sha256 = "09w4glff27sw4z2998gpq5vmlv36mfx9vp287spm7xvaq9fnn6gb";
-    };
-  };
-in
 {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
-  home.packages = with pkgs; [
-    ripgrep
-    nodejs yarn
+  imports = [
+    ../../modules/neovim.nix
   ];
 
   nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
@@ -43,49 +30,6 @@ in
     export EDITOR="nvim"
     export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
   '';
-
-  programs = {
-    neovim = {
-      enable = true;
-      viAlias = true;
-      vimAlias = true;
-      withNodeJs = true;
-      extraConfig = ''
-        filetype plugin indent on
-        syntax on
-        set backspace=indent,eol,start
-        set hidden
-        set nobackup
-        set nowritebackup
-        set noswapfile
-        set cmdheight=2
-        set updatetime=300
-        set shortmess+=c
-        colorscheme breezy
-        set background=light
-        set number
-
-        let g:lightline = {
-          \ 'colorscheme': 'breezy',
-          \ 'component': {
-          \   'readonly': '%{&readonly?"":""}',
-          \ },
-          \ 'separator': { 'left': '', 'right': '' },
-          \ 'subseparator': { 'left': '', 'right': '' }
-          \ }
-        let g:coc_user_config = {
-          \ 'rust-client.disableRustup': v:true
-          \ }
-      '';
-      plugins = with pkgs.vimPlugins; [
-        lightline-vim coc-nvim vim-nix fzf-vim coc-fzf fzfWrapper breezy
-        coc-java coc-json coc-python coc-rls coc-yaml
-      ];
-    };
-    fzf = {
-      enable = true;
-    };
-  };
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
