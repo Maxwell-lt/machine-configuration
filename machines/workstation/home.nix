d8f8ce749c54f457d072ab404e1df91f6875b3ec
Maxwell L-T <maxwell.lt@live.com>
2020-10-17 18:22:26 -0400

    Merge .zshrc for mediaserver and workstation into external file

--- machines/workstation/home.nix
+++ machines/workstation/home.nix
@@ -16,26 +16,7 @@
   nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
   nixpkgs.config.allowUnfree = true;
 
-  home.file.".zshrc".text = ''
-    # Lines configured by zsh-newuser-install
-    HISTFILE=~/.histfile
-    HISTSIZE=1000
-    SAVEHIST=1000
-    setopt appendhistory autocd notify
-    unsetopt beep
-    bindkey -e
-    # End of lines configured by zsh-newuser-install
-    # The following lines were added by compinstall
-    zstyle :compinstall filename '/home/maxwell/.zshrc'
-    ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=10'
-
-    autoload -Uz compinit
-    compinit
-    # End of lines added by compinstall
-    eval "$(direnv hook zsh)"
-    export EDITOR="nvim"
-    export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --hidden'
-  '';
+  home.file.".zshrc".source = ../../dotfiles/.zshrc;
 
   # Home Manager needs a bit of information about you and the
   # paths it should manage.
