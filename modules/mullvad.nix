4d052c60966a2d5c1644d5e6b87672722227ccd0
Maxwell L-T <maxwell.lt@live.com>
2020-10-25 13:51:05 -0400

    Override mullvad version to latest

--- modules/mullvad.nix
+++ modules/mullvad.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, fetchurl, lib, ... }:
 
 {
   services.mullvad-vpn.enable = true;
@@ -6,6 +6,16 @@
   environment.systemPackages = with pkgs; [
     mullvad-vpn
   ];
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    mullvad-vpn = pkgs.mullvad-vpn.overrideAttrs (old: rec {
+      version = "2020.6";
+      src = pkgs.fetchurl {
+        url = "https://www.mullvad.net/media/app/MullvadVPN-${version}_amd64.deb";
+        sha256 = "0d9rv874avx86jppl1dky0nfq1633as0z8yz3h3f69nhmcbwhlr3";
+      };
+    });
+  };
 
   # Workaround for NixOS/nixpkgs#91923
   networking.iproute2.enable = true;
