ac02eb64cd265a510a0a2b0ee7cf4bdcb88a0adb
Maxwell L-T <maxwell.lt@live.com>
2020-11-08 23:54:14 -0500

    Add home-manager to laptop

---
+++ machines/laptop/home.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  imports = [
+    ../../modules/neovim.nix
+  ];
+
+  home.packages = with pkgs; [
+    #lutris
+    playonlinux
+  ];
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "library-of-babel" = {
+        hostname = "maxwell-lt.dev";
+        port = 22;
+        user = "maxwell";
+      };
+      "media-server-alpha" = {
+        hostname = "10.0.0.114";
+        port = 22;
+        user = "maxwell";
+      };
+    };
+  };
+
+  nixpkgs.config.firefox.enablePlasmaBrowserIntegration = true;
+  nixpkgs.config.allowUnfree = true;
+
+  home.file.".zshrc".source = ../../dotfiles/.zshrc;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "maxwell";
+  home.homeDirectory = "/home/maxwell";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.09";
+}
