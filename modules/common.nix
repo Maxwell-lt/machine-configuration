3786acc4e1d885a8991c53cadb5d54b008bf2bb0
Maxwell L-T <maxwell.lt@live.com>
2020-11-11 23:27:41 -0500

    Fix slow paste into zsh
    Solution from: https://github.com/zsh-users/zsh-autosuggestions/issues/238#issuecomment-389324292

--- modules/common.nix
+++ modules/common.nix
@@ -118,6 +118,18 @@ in
           pkgs.nix-zsh-completions
         ];
       };
+      shellInit = ''
+        pasteinit() {
+          OLD_SELF_INSERT=''${''${(s.:.)widgets[self-insert]}[2,3]}
+          zle -N self-insert url-quote-magic # I wonder if you'd need `.url-quote-magic`?
+        }
+
+        pastefinish() {
+          zle -N self-insert $OLD_SELF_INSERT
+        }
+        zstyle :bracketed-paste-magic paste-init pasteinit
+        zstyle :bracketed-paste-magic paste-finish pastefinish
+      '';
     };
   };
 
