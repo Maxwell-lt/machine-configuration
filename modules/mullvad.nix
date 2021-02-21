3d75d70a99d1fedf76409a55b1926b3512350efe
Maxwell L-T <maxwell.lt@live.com>
2021-02-20 23:56:26 -0500

    Revert to older Mullvad version due to NixOS/nixpkgs#113589

--- modules/mullvad.nix
+++ modules/mullvad.nix
@@ -9,4 +9,16 @@
 
   # Workaround for NixOS/nixpkgs#91923
   networking.iproute2.enable = true;
+
+  networking.wireguard.enable = true;
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    mullvad-vpn = pkgs.mullvad-vpn.overrideAttrs (old: rec {
+      version = "2020.7";
+      src = pkgs.fetchurl {
+        url = "https://www.mullvad.net/media/app/MullvadVPN-${version}_amd64.deb";
+        sha256 = "07vryz1nq8r4m5y9ry0d0v62ykz1cnnsv628x34yvwiyazbav4ri";
+      };
+    });
+  };
 }
