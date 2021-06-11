1ff4bfa0cf5c05372bffb67390a0a6e80403d4bd
Maxwell L-T <maxwell.lt@live.com>
2021-06-10 21:17:32 -0400

    Switch to PipeWire

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -103,9 +103,18 @@
   # Enable sound.
   sound.enable = true;
   hardware.pulseaudio = {
-    enable = true;
+    enable = false;
     package = pkgs.pulseaudioFull;
     extraModules = [ pkgs.pulseaudio-modules-bt ];
+  };
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = true;
   };
 
   # Enable bluetooth.
