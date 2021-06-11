0f1b3970300db54fa12350fc0f937afddbf41095
Maxwell L-T <maxwell.lt@live.com>
2021-06-10 21:17:49 -0400

    Apply workarounds and update to latest Mullvad version

--- modules/mullvad.nix
+++ modules/mullvad.nix
@@ -2,23 +2,13 @@
 
 {
   services.mullvad-vpn.enable = true;
-
+  
   environment.systemPackages = with pkgs; [
     mullvad-vpn
   ];
 
-  # Workaround for NixOS/nixpkgs#91923
-  networking.iproute2.enable = true;
-
   networking.wireguard.enable = true;
 
-  nixpkgs.config.packageOverrides = pkgs: {
-    mullvad-vpn = pkgs.mullvad-vpn.overrideAttrs (old: rec {
-      version = "2020.7";
-      src = pkgs.fetchurl {
-        url = "https://www.mullvad.net/media/app/MullvadVPN-${version}_amd64.deb";
-        sha256 = "07vryz1nq8r4m5y9ry0d0v62ykz1cnnsv628x34yvwiyazbav4ri";
-      };
-    });
-  };
+  # Until nixpkgs/pull/121906 is merged
+  networking.firewall.checkReversePath = "loose";
 }
