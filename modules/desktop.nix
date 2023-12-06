8852bb1042510702152b7fbdaf097dbe379cf5e8
Maxwell L-T <maxwell.lt@live.com>
2023-12-06 17:18:41 -0500

    Desktop: fcitx environment variables are set by module

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -184,12 +184,5 @@
       defaultSession = "plasmawayland";
     };
     desktopManager.plasma5.enable = true;
-    # Enable Japanese IME
-    displayManager.sessionCommands = ''
-      export XMODIFIERS="@im=fcitx"
-      export XMODIFIER="@im=fcitx"
-      export GTK_IM_MODULE="fcitx"
-      export QT_IM_MODULE="fcitx"
-    '';
   };
 }
