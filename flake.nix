af48614f1496f7e22d1b0c9c77eab45c1b5b5ae5
Maxwell L-T <maxwell.lt@live.com>
2023-12-23 16:59:13 -0500

    Flakify home-manager, xclip -> wl-clipboard

--- flake.nix
+++ flake.nix
@@ -11,43 +11,65 @@
     nixified-ai = {
       url = "github:nixified-ai/flake";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, sops-nix, nixified-ai }: {
-    nixosConfigurations = 
-    let
-      buildSystem = config: nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = config ++ [ sops-nix.nixosModules.sops ];
-        specialArgs = { inherit nixified-ai; };
+  outputs = inputs@{ self, nixpkgs, sops-nix, nixified-ai, home-manager }: {
+    nixosConfigurations =
+      let
+        linux64System = "x86_64-linux";
+        buildSystem = config: nixpkgs.lib.nixosSystem {
+          system = linux64System;
+          modules = config ++ [ sops-nix.nixosModules.sops ];
+          specialArgs = { inherit nixified-ai; };
+        };
+      in
+      {
+        maxwell-nixos = buildSystem [
+          ./machines/workstation/configuration.nix
+        ];
+        media-server-alpha = buildSystem [
+          ./machines/mediaserver/configuration.nix
+          nixified-ai.nixosModules.invokeai
+          {
+            environment.systemPackages = [ nixified-ai.packages.x86_64-linux.textgen-nvidia ];
+            nixpkgs.overlays = [
+              (final: prev: {
+                invokeai-nvidia = nixified-ai.packages.x86_64-linux.invokeai-nvidia;
+              })
+            ];
+            nix.settings = {
+              trusted-substituters = [ "https://ai.cachix.org" ];
+              trusted-public-keys = [ "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc=" ];
+            };
+          }
+        ];
+        nix-portable-omega = buildSystem [
+          ./machines/laptop/configuration.nix
+        ];
+        library-of-babel = buildSystem [
+          ./machines/library-of-babel/configuration.nix
+        ];
       };
-    in
-    {
-      maxwell-nixos = buildSystem [
-        ./machines/workstation/configuration.nix
-      ];
-      media-server-alpha = buildSystem [
-        ./machines/mediaserver/configuration.nix
-        nixified-ai.nixosModules.invokeai
-        { 
-          environment.systemPackages = [ nixified-ai.packages.x86_64-linux.textgen-nvidia ];
-          nixpkgs.overlays = [
-            (final: prev: {
-              invokeai-nvidia = nixified-ai.packages.x86_64-linux.invokeai-nvidia;
-            })
-          ];
-          nix.settings = {
-            trusted-substituters = ["https://ai.cachix.org"];
-            trusted-public-keys = ["ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc="];
-          };
-        }
-      ];
-      nix-portable-omega = buildSystem [
-        ./machines/laptop/configuration.nix
-      ];
-      library-of-babel = buildSystem [
-        ./machines/library-of-babel/configuration.nix
-      ];
-    };
+    homeConfigurations =
+      let
+        linux64System = "x86_64-linux";
+        pkgs = nixpkgs.legacyPackages.${linux64System};
+        buildHome = config: home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = config;
+        };
+      in
+      {
+        "maxwell@maxwell-nixos" = buildHome [
+          machines/workstation/home.nix
+        ];
+        "maxwell@media-server-alpha" = buildHome [
+          machines/mediaserver/home.nix
+        ];
+      };
   };
 }
