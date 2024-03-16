9332e7dfeed43f59b01e136df1863c51a0b7de70
Maxwell L-T <maxwell.lt@live.com>
2024-03-15 21:59:35 -0400

    Add hyprland cache

--- flake.nix
+++ flake.nix
@@ -26,6 +26,12 @@
           modules = config ++ [ 
             sops-nix.nixosModules.sops
             ./config
+            {
+                nix.settings = {
+                  substituters = ["https://hyprland.cachix.org"];
+                  trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+                };
+            }
           ];
         };
       in
