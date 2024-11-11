2359db2a66f0dd936fc86ad9e860fffe12c16477
Maxwell L-T <maxwell.lt@live.com>
2024-11-11 02:15:36 -0500

    media-server: fix typos in Authelia config, temp disable OIDC secrets

--- config/authelia.nix
+++ config/authelia.nix
@@ -38,15 +38,14 @@ in
       enable = true;
       secrets = {
         jwtSecretFile = secrets.authelia_jwt_secret.path;
-        oidcHmacSecretFile = secrets.authelia_hmac_secret.path;
-        oidcIssuerPrivateKeyFile = secrets.authelia_issuer_priv_key.path;
+        #oidcHmacSecretFile = secrets.authelia_hmac_secret.path;
+        #oidcIssuerPrivateKeyFile = secrets.authelia_issuer_priv_key.path;
         sessionSecretFile = secrets.authelia_session_secret.path;
         storageEncryptionKeyFile = secrets.authelia_storage_encryption_key.path;
       };
       environmentVariables = {
-        AUTHELIA_NOTIFIER_SMTP_USERNAME_FILE = secrets.authelia_smtp_username.path;
         AUTHELIA_NOTIFIER_SMTP_PASSWORD_FILE = secrets.authelia_smtp_password.path;
-        AUTHELIA_AUTHENTICATION_BACKEND_LDAP_PASSSWORD_FILE = secrets.authelia_ldap_password.path;
+        AUTHELIA_AUTHENTICATION_BACKEND_LDAP_PASSWORD_FILE = secrets.authelia_ldap_password.path;
         AUTHELIA_DUO_API_SECRET_KEY_FILE = secrets.authelia_duo_api_key.path;
       };
       settings = {
@@ -74,6 +73,9 @@ in
             user = "cn=authelia_bind_user,dc=maxwell-lt,dc=dev";
           };
         };
+        access_control = {
+          default_policy = "two_factor";
+        };
         storage = {
           postgres = {
             address = "unix:///run/postgresql";
@@ -84,15 +86,21 @@ in
           };
         };
         session = {
-          domain = "maxwell-lt.dev";
-          authelia_url = "https://auth.maxwell-lt.dev";
           redis = {
             host = "/run/redis-authelia/redis.sock";
           };
+          cookies = [
+            {
+              domain = "maxwell-lt.dev";
+              authelia_url = "https://auth.maxwell-lt.dev";
+              default_redirection_url = "https://www.maxwell-lt.dev";
+            }
+          ];
         };
         server.endpoints.authz.forward-auth.implementation = "ForwardAuth";
-        notifier = {
+        notifier.smtp = {
           address = "submission://smtp.gmail.com:587";
+          username = "{{secret \"${secrets.authelia_smtp_username.path}\" }}";
           sender = "maxwell-lt.dev Auth Server <noreply@auth.maxwell-lt.dev>";
         };
       };
