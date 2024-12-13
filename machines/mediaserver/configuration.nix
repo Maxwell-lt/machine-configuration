64d5ba7b6706e77be56d51960f68057ae8b29ece
Maxwell L-T <maxwell.lt@live.com>
2024-12-12 22:48:35 -0500

    mediaserver: remove Minecraft configuration

--- machines/mediaserver/configuration.nix
+++ machines/mediaserver/configuration.nix
@@ -167,25 +167,6 @@
         proxyPass = "http://10.0.0.156:9811";
       };
     };
-    virtualHosts."serverpack" = {
-      serverName = "10.100.0.2";
-      listen = [{
-        addr = "10.100.0.2";
-        port = 9990;
-      }];
-      locations."/" = {
-        root = "/srv/minecraft";
-      };
-      extraConfig = ''
-        autoindex on;
-      '';
-    };
-  };
-
-  fileSystems."/srv/minecraft" = {
-    device = "/home/minecraft/web";
-    depends = ["/home/minecraft/web"];
-    options = ["bind"];
   };
 
   # Needed for tc to work in the firewall script
@@ -199,13 +180,6 @@
     9100 9101 9102 9811 9812
     # InvokeAI
     9090
-
-    # Dynmap
-    8123
-    # Minecraft
-    25565
-    # Minecraft web site
-    9990
   ];
 
   # Setup Wireguard client
@@ -257,23 +231,6 @@
     enableAlarm = true;
     turnUPSOff = false;
     hibernate = false;
-  };
-
-  users.users.minecraft = {
-    description  = "Minecraft user account";
-    isNormalUser = true;
-    shell = pkgs.zsh;
-    createHome = true;
-    linger = true;
-    packages = with pkgs; [
-      tmux
-    ];
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQD5LS315i42OMhMxkRjLrvdP65zDYdlD0hAOjXslf6JAhgvQu7CUbnLmMhivKlXp7z825NWrB66jlR5R6muO6bwoSDC9RID01ixcRv1iF4fmveDDXkSUy1MjeOdUOcml+zhh+IIi/SkGsjI7weqe0fKJCj1uIoru+UoIOjPeL0uC32Sl/GC9VRVGqiH57lIjkUaf9j3Ja9MvY63nx5W1+BIQuOlabEB2XD8hIUiEQEi0jNCCkAuvhnJjHIGSIRvUQBijInUGOR7M8eRmEwTrbl36DFIphnKKP+mhAefy5zIIMctdDucqyfweizLBg2D4qY1WiXHflng5k63h5WRYwvAyLJQ7Jy9/Dvm2eNYWhQ0bdGV0a3l3oRvthIReXgLuygWs9M/quCyb1VnNRYbxs1vRwI1MzN1EZ7W8OfX/5S3XNy3DBENoga4eA8xXanhSM3StRFpaYfx05E4x2tdQYQ2CMbps14oMEZ8bYc1cxD5r1aDfzzo2/0YkLGhVJVpoBrmCaQsHc07klqb+XwWTkqxdE/jTiNV3ZXXHlzD3Vt1jD8Goo2kMqjC1MGwFTUJAz20St3O/3ntka1sYZZJ8dDzU/ly6xI3xW1IN+o0A7Q9qpIIw4Lgc1eWEURH3/D2fnDTcFPIIh9h1oEEXldl0j6dWrw8f3XySBVBk7yNJzB0/Q== maxwell.lt@live.com"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE6RnS6RiN5u9vyXVKMZgnCsLJOuXaqADbDQWfShufCv maxwell@nix-portable-omega"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO/qNB6f8IaU2BuI9AsHodHuOoaPabGNogUJQUs2etXE maxwell@pixel"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2M9+aenrZ9xCtrF1zsQTgmUeQjj5mzSgD6Y9lARWB+ JuiceSSH"
-    ];
   };
 
   nix.settings.sandbox = "relaxed";
