e09aac600ff0c42209de6713d09fbb5a3f362837
Maxwell L-T <maxwell.lt@live.com>
2026-05-23 22:31:53 -0400

    hyprland: use start-hyprland to launch

--- config/desktop.nix
+++ config/desktop.nix
@@ -142,7 +142,7 @@ in
         enable = true;
         settings = {
           default_session = {
-            command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd Hyprland";
+            command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd start-hyprland";
           };
         };
       };
